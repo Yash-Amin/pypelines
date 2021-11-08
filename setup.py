caff4d1ee604b183f88acb8bf394a851ace10cd0
Yash Amin <aminyash8@gmail.com>
2021-11-08 09:18:37 +0530

    init

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="pypelines",
+    version="1.0.0",
+    author="Yash",
+    author_email="aminyash8@gmail.com",
+    # TODO: update other fields
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
