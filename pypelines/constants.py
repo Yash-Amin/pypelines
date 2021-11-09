7e70178eb7a1eb9f3937965755ff372a38e4c5ea
Yash Amin <aminyash8@gmail.com>
2021-11-09 08:46:51 +0530

    Add constants

---
+++ pypelines/constants.py
@@ -0,0 +1,12 @@
+"""Constants"""
+
+
+class SnapshotCollectionFields:
+    """Snapshot collection field names"""
+
+    PIPELINE_ID = "pipeline_id"
+    PIPELINE_NAME = "pipeline_name"
+    CREATED_AT = "created_at"
+    RECORD_ID = "_id"
+    COMPLETED_TASKS = "completed_tasks"
+    IS_COMPLETED = "is_completed"
