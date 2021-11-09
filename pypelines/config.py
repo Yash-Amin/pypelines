2d3c984e69880ac76b24a9c33b6ef103ea5cdc37
Yash Amin <aminyash8@gmail.com>
2021-11-09 08:47:03 +0530

    Modify config param names

--- pypelines/config.py
+++ pypelines/config.py
@@ -8,12 +8,12 @@ WORKSPACE_DIRECTORY = os.path.expanduser("~/.Pypelines/")
 SCRIPTS_DIRECTORY = os.path.join(WORKSPACE_DIRECTORY, "snapshots")
 
 # DB connection string
-DATABASE_CONNECTION_STRING = os.environ.get(
-    "DATABASE_CONNECTION_STRING", "mongodb://localhost:27017/"
+DB_CONNECTION_STRING = os.environ.get(
+    "DB_CONNECTION_STRING", "mongodb://localhost:27017/"
 )
 
 # Database name
-DB_NAME = os.environ.get("DATABASE_NAME", "Pypelines")
+DB_NAME = os.environ.get("DB_NAME", "Pypelines")
 
 # Snapshots collection name
 DB_SNAPSHOT_COLLECTION_NAME = os.environ.get("DB_SNAPSHOT_COLLECTION_NAME", "Snapshots")
